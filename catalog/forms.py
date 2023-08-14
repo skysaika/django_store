6bc08a27e185d2cf1db67e8c46b9e4ffaa76666a
saysuu <skysaika@yandex.ru>
2023-08-14 20:10:00 +0300

    create forms.py add ProductForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,32 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
-    """Форма для создания продукта в форме"""
+    """Форма для создания и редактирования продукта"""
     class Meta:
         model = Product
         fields = ['title', 'description', 'image', 'category', 'price', 'in_stock']
+
+    def clean_title(self):
+        """Проверяет название продукта на наличие запрещенных слов.
+        Если в названии запрещенное слово -> forms.ValidationError
+        """
+        title = self.cleaned_data['title']
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        for word in forbidden_words:
+            if word in title.lower():
+                raise forms.ValidationError("Нельзя добавлять запрещенные слова в название продукта.")
+        return title
+
+    def clean_description(self):
+        """
+        Проверяет описание продукта на наличие запрещенных слов.
+        Если в описании запрещенное слово -> forms.ValidationError
+        """
+        description = self.cleaned_data['description']
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                           'радар']
+        for word in forbidden_words:
+            if word in description.lower():
+                raise forms.ValidationError("Нельзя добавлять запрещенные слова в описание продукта.")
+        return description
