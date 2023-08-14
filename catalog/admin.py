dac830d2fd69014396c96743ffebfc02a8010ccc
saysuu <skysaika@yandex.ru>
2023-08-14 20:04:03 +0300

    change ProductAdmin to VersionAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -14,12 +14,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'category', 'price', 'in_stock')
     list_display_links = ('title',)
     list_editable = ('price',)
-    list_filter = ('in_stock', 'category')
+    list_filter = ('in_stock', 'category', )
     search_fields = ('title', 'description',)
 
 
 @admin.register(Version)
-class ProductAdmin(admin.ModelAdmin):
+class VersionAdmin(admin.ModelAdmin):
     list_display = ('product', 'version_name', 'is_active_version',)
     list_filter = ('version_name', 'is_active_version',)
 
