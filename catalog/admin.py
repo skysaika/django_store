f1bc5f5512e463de133657d17165fce6805153b0
saysuu <skysaika@yandex.ru>
2023-08-17 00:38:07 +0300

    change admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,24 +4,26 @@ from catalog.models import Category, Product, Version
 
 
 # Register your models here.
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title',)
-    list_display_links = ('title',)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'category', 'price', 'in_stock')
+    list_display = ('pk', 'title', 'category', 'price', 'in_stock',)
     list_display_links = ('title',)
     list_editable = ('price',)
     list_filter = ('in_stock', 'category', )
     search_fields = ('title', 'description',)
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title',)
+    list_display_links = ('title',)
+
+
 @admin.register(Version)
 class VersionAdmin(admin.ModelAdmin):
-    list_display = ('product', 'version_name', 'is_active_version',)
-    list_filter = ('version_name', 'is_active_version',)
+    list_display = ('product', 'version_name', 'version_number', 'is_active_version',)
+    list_filter = ('is_active_version',)
 
 
 
