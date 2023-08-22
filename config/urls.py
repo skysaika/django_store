4122667d7c391f6d0b76ebffabb01cda5d520b6f
saysuu <skysaika@yandex.ru>
2023-08-22 22:49:57 +0300

    create menu link 'Войти'

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', 'catalog')),
     path('blog/', include('blog.urls', 'blog')),
+    path('users/', include('users.urls', 'users')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
