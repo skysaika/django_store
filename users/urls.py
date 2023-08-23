f4bdeef7cab045db944c0885a79989b3cfd0096e
saysuu <skysaika@yandex.ru>
2023-08-23 20:58:53 +0300

    create menu link 'Выйти' and form login/logout

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
@@ -7,4 +7,6 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
